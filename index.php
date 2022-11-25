4d103ade91824e5b45f8659e81e1614b0faa18ce
SevenErol <lorenzo.maggi1998@gmail.com>
2022-11-25 14:21:31 +0100

    initial commit basic structure

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words PHP</title>
+
+    <!-- link bootstrap  -->
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT' crossorigin='anonymous'>
+
+</head>
+<body>
+
+<div class="container text-center p-3">
+    <form action="badwords.php" method="get">
+
+    <h1>Put your text here!</h1>
+        <div>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+
+        <h2>select your censored word</h2>
+        <input type="text" class="mb-3">
+
+        <div>
+        <button type="button" class="btn btn-primary">Submit</button>
+        </div>
+        
+    </form>
+</div>
+
+<!-- link bootstrap  -->
+<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT' crossorigin='anonymous'>
+    
+</body>
+</html>
