9c2fa328660c4d355745c8af0c470a1ac4659d5a
SevenErol <lorenzo.maggi1998@gmail.com>
2022-11-25 15:01:50 +0100

    now complete exercise

--- badwords.php
+++ badwords.php
@@ -6,10 +6,14 @@ $word = $_GET["word"];
 
 $new_string = str_ireplace($word, "***", $paragraph);
 
+echo $paragraph;
+
+echo strlen($paragraph);
+
 echo $new_string;
 
 
 
 ?>
 
-<h1>Your censored paragraph is: <?php echo $new_string; ?></h1>
+<h1>Your censored paragraph is: <?php echo $new_string; ?> length is: <?php echo strlen($new_string)?></h1>
